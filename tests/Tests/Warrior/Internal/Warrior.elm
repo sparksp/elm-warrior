2b08cba05f667c7b6938a9a5625ecafcb02df54f
Phill Sparks <me@phills.me.uk>
2020-06-14 09:43:37 +0100

    Test Warrior.Internal.Warrior.spawnVillian

--- tests/Tests/Warrior/Internal/Warrior.elm
+++ tests/Tests/Warrior/Internal/Warrior.elm
@@ -9,6 +9,7 @@ all : Test
 all =
     describe "Internal.Warrior"
         [ describe "spawnHero" spawnHeroTests
+        , describe "spawnVillain" spawnVillainTests
         ]
 
 
@@ -19,7 +20,7 @@ initialHealth =
 
 spawnHeroTests : List Test
 spawnHeroTests =
-    [ test "returns a warrior with the given id and coords" <|
+    [ test "returns a Hero with the given id and coords" <|
         \() ->
             let
                 name =
@@ -33,6 +34,33 @@ spawnHeroTests =
                     [ Player.id >> Expect.equal name
                     , Player.position >> Expect.equal coords
                     , Player.isHero >> Expect.equal True
+                    , Player.isVillain >> Expect.equal False
+
+                    -- Defaults
+                    , Player.health >> Expect.equal initialHealth
+                    , Player.maxHealth >> Expect.equal initialHealth
+                    , Player.inventory >> List.isEmpty >> Expect.equal True
+                    ]
+    ]
+
+
+spawnVillainTests : List Test
+spawnVillainTests =
+    [ test "returns a Villain with the given id and coords" <|
+        \() ->
+            let
+                name =
+                    "evil"
+
+                coords =
+                    { x = 1, y = 1 }
+            in
+            Player.spawnVillain name coords
+                |> Expect.all
+                    [ Player.id >> Expect.equal name
+                    , Player.position >> Expect.equal coords
+                    , Player.isVillain >> Expect.equal True
+                    , Player.isHero >> Expect.equal False
 
                     -- Defaults
                     , Player.health >> Expect.equal initialHealth
