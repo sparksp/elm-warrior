91d34ed0ea4946693d7393b1650c92b7b2e47a19
Phill Sparks <me@phills.me.uk>
2020-06-14 22:06:48 +0100

    Test Warrior.Internal.Warrior.heal & healingPotential

--- tests/Tests/Warrior/Internal/Warrior.elm
+++ tests/Tests/Warrior/Internal/Warrior.elm
@@ -15,6 +15,8 @@ all =
         , describe "withPosition" withPositionTests
         , describe "attack" attackTests
         , describe "attackDamage" attackDamageTests
+        , describe "heal" healTests
+        , describe "healingPotential" healingPotentialTests
         ]
 
 
@@ -151,6 +153,99 @@ attackDamageTests =
     ]
 
 
+healTests : List Test
+healTests =
+    [ test "with 1 health and no potion heals by 1" <|
+        \() ->
+            playerWithHealth 1
+                |> Player.heal
+                |> Player.health
+                |> Expect.equal 2
+    , test "healing does not pass max health (10)" <|
+        \() ->
+            testPlayer
+                |> Player.heal
+                |> Player.health
+                |> Expect.equal 10
+    , test "with a potion, uses potion and heals by 6" <|
+        \() ->
+            playerWithHealth 1
+                |> Player.addItem Item.Potion
+                |> Player.heal
+                |> Expect.all
+                    [ Player.health >> Expect.equal 7
+                    , Player.inventory >> Expect.equalLists []
+                    ]
+    , test "with 2 potions, uses 1 potion and heals by 6" <|
+        \() ->
+            playerWithHealth 1
+                |> Player.addItem Item.Potion
+                |> Player.addItem Item.Potion
+                |> Player.heal
+                |> Expect.all
+                    [ Player.health >> Expect.equal 7
+                    , Player.inventory >> Expect.equalLists [ Item.Potion ]
+                    ]
+    , test "with high health and a potion, uses potion to heal to full" <|
+        \() ->
+            playerWithHealth 8
+                |> Player.addItem Item.Potion
+                |> Player.heal
+                |> Expect.all
+                    [ Player.health >> Expect.equal 10
+                    , Player.inventory >> Expect.equalLists []
+                    ]
+    , test "with full health and a potion, uses the potion" <|
+        \() ->
+            testPlayer
+                |> Player.addItem Item.Potion
+                |> Player.heal
+                |> Expect.all
+                    [ Player.health >> Expect.equal 10
+                    , Player.inventory >> Expect.equalLists []
+                    ]
+    ]
+
+
+healingPotentialTests : List Test
+healingPotentialTests =
+    [ test "with no potions is 1" <|
+        \() ->
+            testPlayer
+                |> Player.healingPotential
+                |> Expect.equal 1
+    , test "with a potion is 6" <|
+        \() ->
+            testPlayer
+                |> Player.addItem Item.Potion
+                |> Player.healingPotential
+                |> Expect.equal 6
+    , test "with many potions is 6" <|
+        \() ->
+            testPlayer
+                |> Player.addItem Item.Potion
+                |> Player.addItem Item.Potion
+                |> Player.addItem Item.Potion
+                |> Player.healingPotential
+                |> Expect.equal 6
+    ]
+
+
 testPlayer : Player.Warrior
 testPlayer =
     Player.spawnHero "test" { x = 0, y = 0 }
+
+
+playerWithHealth : Int -> Player.Warrior
+playerWithHealth health =
+    testPlayer
+        |> injurePlayerUntilHealthIs health
+
+
+injurePlayerUntilHealthIs : Int -> Player.Warrior -> Player.Warrior
+injurePlayerUntilHealthIs health player =
+    if Player.health player > health then
+        injurePlayerUntilHealthIs health (Player.attack testPlayer player)
+
+    else
+        player
