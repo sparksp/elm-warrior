810b798c982e24cd55458c9762dd2b5adf2a4b58
Phill Sparks <me@phills.me.uk>
2020-06-17 22:49:58 +0100

    Test Warrior.Map.Builder.withDescription

--- tests/Tests/Warrior/Map/Builder.elm
+++ tests/Tests/Warrior/Map/Builder.elm
@@ -2,6 +2,7 @@ module Tests.Warrior.Map.Builder exposing (all)
 
 import Expect
 import Test exposing (Test, describe, test)
+import Warrior.Internal.Map exposing (Map(..))
 import Warrior.Map.Builder as Builder
 
 
@@ -9,6 +10,7 @@ all : Test
 all =
     describe "Warrior.Internal.Builder"
         [ describe "spawnPoints" spawnPointsTests
+        , describe "withDescription" withDescriptionTests
         ]
 
 
@@ -32,3 +34,24 @@ spawnPointsTests =
                 |> Builder.spawnPoints
                 |> Expect.equalLists [ { x = 2, y = 3 } ]
     ]
+
+
+withDescriptionTests : List Test
+withDescriptionTests =
+    [ test "adds description to the map" <|
+        \() ->
+            Builder.init { rows = 1, columns = 1 }
+                |> Builder.withDescription "A test map."
+                |> Builder.build
+                |> mapDescription
+                |> Expect.equal "A test map."
+    ]
+
+
+
+--- HELPERS
+
+
+mapDescription : Map -> String
+mapDescription (Map map) =
+    map.description
