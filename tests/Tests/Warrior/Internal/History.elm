0749fa7346beceec99af894848a58720858d36ab
Phill Sparks <me@phills.me.uk>
2020-06-19 09:40:25 +0100

    Test Warrior.Internal.History.previousActions

--- tests/Tests/Warrior/Internal/History.elm
+++ tests/Tests/Warrior/Internal/History.elm
@@ -2,14 +2,19 @@ module Tests.Warrior.Internal.History exposing (all)
 
 import Expect
 import Test exposing (Test, describe, test)
+import Warrior
+import Warrior.Direction as Direction
 import Warrior.Internal.History as History
+import Warrior.Internal.Map exposing (Map)
 import Warrior.Internal.Warrior as Player
+import Warrior.Map.Builder as Builder
 
 
 all : Test
 all =
     describe "Warrior.Internal.History"
         [ describe "init" initTests
+        , describe "previousActions" previousActionsTests
         ]
 
 
@@ -37,6 +42,50 @@ initTests =
     ]
 
 
+previousActionsTests : List Test
+previousActionsTests =
+    [ test "with no actions is an empty list" <|
+        \() ->
+            History.init
+                |> History.previousActions testPlayer
+                |> Expect.equal []
+    , test "only gets actions for the given player with most recent first" <|
+        \() ->
+            let
+                phill : Player.Warrior
+                phill =
+                    Player.spawnHero "Phill" { x = 0, y = 0 }
+
+                robin : Player.Warrior
+                robin =
+                    Player.spawnHero "Robin" { x = 4, y = 4 }
+
+                map : Map
+                map =
+                    emptyMap
+            in
+            History.init
+                |> History.record robin map (Warrior.Move Direction.Left)
+                |> History.record phill map (Warrior.Move Direction.Right)
+                |> History.record (robin |> Player.withPosition { x = 3, y = 4 }) map (Warrior.Move Direction.Up)
+                |> History.record (phill |> Player.withPosition { x = 1, y = 0 }) map (Warrior.Move Direction.Down)
+                |> History.record (robin |> Player.withPosition { x = 3, y = 3 }) map Warrior.Wait
+                |> Expect.all
+                    [ History.previousActions phill
+                        >> Expect.equalLists
+                            [ Warrior.Move Direction.Down
+                            , Warrior.Move Direction.Right
+                            ]
+                    , History.previousActions robin
+                        >> Expect.equalLists
+                            [ Warrior.Wait
+                            , Warrior.Move Direction.Up
+                            , Warrior.Move Direction.Left
+                            ]
+                    ]
+    ]
+
+
 
 --- HELPERS
 
@@ -44,3 +93,9 @@ initTests =
 testPlayer : Player.Warrior
 testPlayer =
     Player.spawnHero "Tester" { x = 0, y = 0 }
+
+
+emptyMap : Map
+emptyMap =
+    Builder.init { rows = 5, columns = 5 }
+        |> Builder.build
